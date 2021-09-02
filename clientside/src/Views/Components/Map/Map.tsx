c84e28da4d2671450b1dc2b57a0f00156bbf28a2
jamestkelly <jimkelly.t@outlook.com>
2021-09-02 20:34:59 +1000

    Small change in comments

--- clientside/src/Views/Components/Map/Map.tsx
+++ clientside/src/Views/Components/Map/Map.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Layer, Source } from 'react-map-gl';
 
 import { MAP_TOKEN } from 'Constants/Tokens';
+import * as mapData from 'Packages/Data/geo.json';
+import * as d3 from 'd3';
 
 export default function Map() {
     const [viewport, setViewport] = useState({
         latitude: -22.164678,
         longitude: 144.5844903,
         width: '100vw',
-        height: '93.85vh', // Temporarily hard set for development
+        height: '93.85vh', // Temporarily hard coded for development
         zoom: 5,
         pitch: 30,
     });
+
+    const data = d3.json('Packages/Data/geo.json');
 
     return (
         <div>
@@ -24,7 +28,13 @@ export default function Map() {
                     setViewport(viewport);
                 }}
             >
-                Markers here
+                <Source id="qldboundaries" type="geojson" data={ data }/>
+                <Layer
+                    id="test"
+                    type="fill"
+                    source="qldboundaries"
+                    paint={{'fill-color': '#228b22', 'fill-opacity': 0.4}}
+                />
             </ReactMapGL>
         </div>
     );
