8d183b2e740dce447e2440ef42da4eaa85d93401
jamestkelly <jimkelly.t@outlook.com>
2021-09-11 16:53:33 +1000

    Small Fix so map will display
    Removed the data so the map can display

--- clientside/src/Views/Components/Map/Map.tsx
+++ clientside/src/Views/Components/Map/Map.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useMemo, Component } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import ReactMapGL, { Layer, Source } from 'react-map-gl';
 
 import { MAP_TOKEN } from 'Constants/Tokens';
-import * as mapData from 'Packages/Data/geo.json';
-import * as d3 from 'd3';
+//import * as d3 from 'd3';
+
 
 export default function Map() {
+    //const mapData = require("../../../../public/data/geo.json");
     const [viewport, setViewport] = useState({
         latitude: -22.164678,
         longitude: 144.5844903,
@@ -15,8 +16,17 @@ export default function Map() {
         zoom: 5,
         pitch: 30,
     });
+    const [allData, setAllData] = useState(null);
+    const [data, setData] = useState(null)
 
-    const data = d3.json('Packages/Data/geo.json');
+    //const data = d3.json('Packages/Data/geo.json');
+
+    // Fetch the map data from public directory (can be changed to online link)
+    useEffect(() => {
+        fetch("../../../../public/data/geo.json")
+            .then(resp => resp.json())
+            .then(json => setAllData(json));
+    }, []);
 
     return (
         <div>
@@ -28,12 +38,14 @@ export default function Map() {
                     setViewport(viewport);
                 }}
             >
-                <Source id="qldboundaries" type="geojson" data={ data }/>
+                <Source id="qldboundaries" type="geojson" data={"../../../../public/data/geo.json"}/>
                 <Layer
                     id="test"
                     type="fill"
                     source="qldboundaries"
-                    paint={{'fill-color': '#228b22', 'fill-opacity': 0.4}}
+                    paint={{
+                        'fill-color': '#e32636'
+                    }}
                 />
             </ReactMapGL>
         </div>
