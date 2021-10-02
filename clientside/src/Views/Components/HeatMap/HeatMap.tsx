9f02f0934998f7dee2f5883cd5a06da4ae392818
jamestkelly <jimkelly.t@outlook.com>
2021-10-02 20:40:14 +1000

    Updated HeatMap.tsx
    Improved resolution of geo.json file & added some comments.

--- clientside/src/Views/Components/HeatMap/HeatMap.tsx
+++ clientside/src/Views/Components/HeatMap/HeatMap.tsx
@@ -2,10 +2,12 @@ import { useState, useEffect } from 'react';
 import MapGL, { Source, Layer } from 'react-map-gl';
 import { DataLayer, LineLayer } from './HeatMapStyle';
 
+// Access token for MapBox API
 const MAPBOX_TOKEN = "pk.eyJ1IjoiamFtZXN0a2VsbHkiLCJhIjoiY2tzOGU4ejl1MG9icjJ1bzJ2MzV6d2xldSJ9.vFX3vaZcZjWnXnqOoMn2Vg";
-const MAP_STYLE = "mapbox://styles/mapbox/light-v10";
+const MAP_STYLE = "mapbox://styles/mapbox/light-v10"; // Map Styling
 
 export default function HeatMapTest() {
+    // Set viewport options for map
     const [viewport, setViewport] = useState({
         latitude: -30.2744,
         longitude: 133.7751,
@@ -13,15 +15,17 @@ export default function HeatMapTest() {
         pitch: 30
     });
 
-    const [data, setData] = useState<any | null>(null);
-    const [hoverInfo, setHoverInfo] = useState(null);
+    const [data, setData] = useState<any | null>(null); // Data set and get object
+    const [hoverInfo, setHoverInfo] = useState(null); // TODO: Add hover information
 
+    // Fetch the data globally
     useEffect(() => {
-        fetch('https://raw.githubusercontent.com/jamestkelly/CommunityMappingData/main/geo2.json')
+        fetch('https://raw.githubusercontent.com/jamestkelly/CommunityMappingData/main/geo-numeric.json')
             .then(resp => resp.json())
             .then(json => setData(json))
     })
     
+    // Render the map component
     return (
         <MapGL
             { ...viewport }
