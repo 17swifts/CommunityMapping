2c12082d0f1c319bf5d3e793a1431726e93bedbe
jamestkelly <jimkelly.t@outlook.com>
2021-11-03 21:18:03 +1000

    Finalised legend :B:

--- clientside/src/Views/Components/HeatMap/HeatMapLegend.tsx
+++ clientside/src/Views/Components/HeatMap/HeatMapLegend.tsx
@@ -1,19 +1,26 @@
-import * as React from 'react';
 import CSS from 'csstype';
 
 export default function HeatMapLegend() {
     const legendStyle: CSS.Properties = {
         background: "#FFFFFF",
-        alignContent: "center"
+    };
+
+    const legendHeader: CSS.Properties = {
+        padding: "5px 0 5px 10px",
+        display: "flex",
+        alignSelf: "center"
     };
 
     const legendRow: CSS.Properties = {
+        padding: "5px 0",
         display: "flex",
-        padding: "10px 0"
+        alignItems: "baseline",
+        justifyContent: "space-between"
     };
 
     const itemText: CSS.Properties = {
-        paddingLeft: "5px"
+        paddingLeft: "5px",
+        color: "#474747"
     };
 
     const boxStyleOne: CSS.Properties = {
@@ -26,12 +33,64 @@ export default function HeatMapLegend() {
     const boxStyleTwo: CSS.Properties = {
         width: "10px",
         height: "10px",
-        backgroundColor: "#EED086"
+        backgroundColor: "#EED086",
+        border: "1px solid #000000"
+    };
+
+    const boxStyleThree: CSS.Properties = {
+        width: "10px",
+        height: "10px",
+        backgroundColor: "#EFBA77",
+        border: "1px solid #000000"
+    };
+
+    const boxStyleFour: CSS.Properties = {
+        width: "10px",
+        height: "10px",
+        backgroundColor: "#EFA669",
+        border: "1px solid #000000"
+    };
+
+    const boxStyleFive: CSS.Properties = {
+        width: "10px",
+        height: "10px",
+        backgroundColor: "#EF9059",
+        border: "1px solid #000000"
+    };
+
+    const boxStyleSix: CSS.Properties = {
+        width: "10px",
+        height: "10px",
+        backgroundColor: "#EF7A4A",
+        border: "1px solid #000000"
+    };
+
+    const boxStyleSeven: CSS.Properties = {
+        width: "10px",
+        height: "10px",
+        backgroundColor: "#EF663C",
+        border: "1px solid #000000"
+    };
+
+    const boxStyleEight: CSS.Properties = {
+        width: "10px",
+        height: "10px",
+        backgroundColor: "#EF522E",
+        border: "1px solid #000000"
+    };
+
+    const boxStyleNine: CSS.Properties = {
+        width: "10px",
+        height: "10px",
+        backgroundColor: "#F03117",
+        border: "1px solid #000000"
     };
 
     return (
         <div style={ legendStyle }>
-            <h5>Gap Score</h5>
+            <div style={ legendHeader }>
+                <h5>Gap Score</h5>
+            </div>
             <div style={ legendRow }>
                 <div style={ boxStyleOne }> </div>
                 <div style={ itemText }>0.00 - 49.99</div>
@@ -39,6 +98,34 @@ export default function HeatMapLegend() {
             <div style={ legendRow }>
                 <div style={ boxStyleTwo }> </div>
                 <div style={ itemText }>50.00 - 99.99</div>
+            </div>
+            <div style={ legendRow }>
+                <div style={ boxStyleThree }> </div>
+                <div style={ itemText }>100.00 - 149.99</div>
+            </div>
+            <div style={ legendRow }>
+                <div style={ boxStyleFour }> </div>
+                <div style={ itemText }>150.00 - 199.99</div>
+            </div>
+            <div style={ legendRow }>
+                <div style={ boxStyleFive }> </div>
+                <div style={ itemText }>200.00 - 249.99</div>
+            </div>
+            <div style={ legendRow }>
+                <div style={ boxStyleSix }> </div>
+                <div style={ itemText }>250.00 - 299.99</div>
+            </div>
+            <div style={ legendRow }>
+                <div style={ boxStyleSeven }> </div>
+                <div style={ itemText }>300.00 - 349.99</div>
+            </div>
+            <div style={ legendRow }>
+                <div style={ boxStyleEight }> </div>
+                <div style={ itemText }>350.00 - 399.99</div>
+            </div>
+            <div style={ legendRow }>
+                <div style={ boxStyleNine }> </div>
+                <div style={ itemText }>400.00 - 449.99</div>
             </div>
         </div>
     )
